ad06187b3fa252ccf0b9503b9d15957314d405fa
sadik <sadikkarki10@gmail.com>
2024-06-25 15:36:06 +0545

    added vercel.json and requirements.txt

--- first_project/settings.py
+++ first_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7ays-fuylb7hfpn(+^v5$v2j-96^-(%v-xyccpmbmto=b658vq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
